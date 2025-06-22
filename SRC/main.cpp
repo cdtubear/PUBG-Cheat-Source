13a9f155fd8cdc157492db08783adb20faca942b
thomasb78 <33074130+thomasb78@users.noreply.github.com>
2025-06-22 22:04:47 +0400

    main.cpp

---
+++ SRC/main.cpp
@@ -0,0 +1,20 @@
+﻿#include "main.h"
+
+int main()
+{
+	MSG Msg;
+
+	AllocConsole();
+	freopen_s((_iobuf**)__acrt_iob_func(1), "conout$", "w", (_iobuf*)__acrt_iob_func(1));
+
+	Game ov1 = Game();
+	ov1.Start();
+	while (::GetMessage(&Msg, 0, 0, 0))
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		::TranslateMessage(&Msg);
+		::DispatchMessage(&Msg);
+	}
+	exit(0);
+	return Msg.wParam;
+}
