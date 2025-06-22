c8a77895303a1f94b5dee692669b760f6361f64a
thomasb78 <33074130+thomasb78@users.noreply.github.com>
2025-06-22 22:04:50 +0400

    Offset.h

---
+++ SRC/Offset.h
@@ -0,0 +1,70 @@
+#pragma once
+#include<windows.h>
+
+
+namespace Offset
+{
+	int Shield = 0x10D96728;
+	int UWorld = 0x127CC8F8;
+	int GNames = 0x12A358B8;
+	int Level = 0x38;
+	int Actor = 0x108;
+	int GameInstence = 0x168;
+	int LocalPlayer = 0xE8;
+	int PlayerController = 0x38;
+	int AcknowledgedPawn = 0x4A8;	
+	int PlayerCameraManager = 0x4D0;
+	int PZ = 0x2;
+	int DecryptOne = 0xAB7DCBE8;
+	int DecryptTwo = 0x95604715;
+	int TableOne = 0x6;
+	int TableTwo = 0x10;
+	int IDD = 0x4010;
+	int ObjID = 0x14;
+	int Position = 0x220;
+	int Playname = 0x11A8;
+	int PlayerState = 0x428;
+	int PlayerSatisitc = 0x4E8;
+	int Bone = 0xAC0;
+	int BoneArry = 0x210;
+	int Mesh = 0x610;
+	int Health = 0xA40;
+	int Health_Die = 0x1BF0;
+	int TeamNumber = 0x34A8;
+	int CameraPos = 0x1008;
+	int CameraRot = 0xA74;
+	int CameraFov = 0xA64;
+	int SeatIndex = 0x230;
+	int Eyes = 0x74C;
+	int LeanLeft = 0xEA4;
+	int LeanRight = 0xEA8;
+	int Veloctity = 0x3E0;
+	int DroppedItemGroup = 0x150;
+	int DroppedItemGroup_Count = 0x158;
+	int DroppedItemGroup_UItem = 0x718;
+	int WeaponProcessor = 0x9A0;
+	int CurrentWeaponIndex = 0x311;
+	int EquippedWeapons = 0x208;
+	int WeaponTrajectoryData = 0x10E0;
+	int TrajectoryConfig = 0x108;
+	int TravelDistance = 0x13C;
+	int TravelDistanceMax = 0x140;
+	int SimulationSubstepTim = 0x148;
+	int VDragCoefficient = 0x14C;
+	int BDS = 0x150;
+	int ControlRotation_CP = 0x7FC;
+	int AnimScriptInstance = 0xC60;
+	int RecoilADSRotation_CP = 0xA9C;
+	int VehicleRiderComponent = 0x20A0;
+	int LastVehiclePawn = 0x270;
+	int ReplicatedMovement = 0x68;
+	int OP0 = 0xB0;
+	int OP01 = 0x268;
+	int OP02 = 0xAC4;
+	int OP03 = 0x12248278;
+	int OP04 = 0x1208E7DC;
+	int OP05 = 0x127E0A00;
+	int OP06 = 0x12A6B25D;
+	int LocalPlayerPTR = 0x1294B4F0;
+	int LineTraceSingle = 0xE3E5640;
+}
